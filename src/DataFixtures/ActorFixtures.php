261064110f79eddc0c17e729176c8622108acaac
Cyril <cydescat@gmail.com>
2020-07-15 10:36:43 +0200

    done ?

--- src/DataFixtures/ActorFixtures.php
+++ src/DataFixtures/ActorFixtures.php
@@ -2,6 +2,7 @@
 
 namespace App\DataFixtures;
 
+use App\Service\Slugify;
 use Faker;
 use App\Entity\Actor;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -12,12 +13,15 @@ class ActorFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
+        $slugify = new Slugify();
         $faker = Faker\Factory::create('fr_FR');
         $i = 0;
         while ($i < 50) {
             $actor = new Actor();
             $actor->setName($faker->name);
             $actor->addProgram($this->getReference("program_4"));
+            $slug = $slugify->generate($actor->getName());
+            $actor->setSlug($slug);
             $manager->persist($actor);
             $i++;
         }
--- src/DataFixtures/ProgramFixtures.php
+++ src/DataFixtures/ProgramFixtures.php
@@ -3,6 +3,7 @@
 namespace App\DataFixtures;
 
 use App\Entity\Program;
+use App\Service\Slugify;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -11,6 +12,7 @@ class ProgramFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager)
     {
+        $slugify = new Slugify();
         $i = 0;
         while ($i < 15) {
             $program = new Program();
@@ -20,6 +22,8 @@ class ProgramFixtures extends Fixture implements DependentFixtureInterface
             $program->setPoster("poster $i");
             $program->setYear(2000 + $i);
             $program->setCategory($this->getReference('category_7'));
+            $slug = $slugify->generate($program->getTitle());
+            $program->setSlug($slug);
             $manager->persist($program);
             $this->addReference("program_$i", $program);
             $i++;
